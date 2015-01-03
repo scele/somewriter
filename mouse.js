b56a6088fc443b3f5f58627fcf217c56b954adbf
Lauri Peltonen <lauri.peltonen@iki.fi>
2015-01-03 19:58:35 +0000

    Inherit InputDevice to Keyboard and Mouse

--- mouse.js
+++ mouse.js
@@ -11,63 +11,6 @@
 var fs = require('fs'),
     EventEmitter = require('events').EventEmitter;
 
-/**
- * Parse PS/2 mouse protocol
- * According to http://www.computer-engineering.org/ps2mouse/
- */
-function parseMouse(dev, buffer) {
-  var event = {
-    leftBtn:    (buffer[0] & 1  ) > 0, // Bit 0
-    rightBtn:   (buffer[0] & 2  ) > 0, // Bit 1
-    middleBtn:  (buffer[0] & 4  ) > 0, // Bit 2
-    xSign:      (buffer[0] & 16 ) > 0, // Bit 4
-    ySign:      (buffer[0] & 32 ) > 0, // Bit 5
-    xOverflow:  (buffer[0] & 64 ) > 0, // Bit 6
-    yOverflow:  (buffer[0] & 128) > 0, // Bit 7
-    xDelta:      buffer.readInt8(1),   // Byte 2 as signed int
-    yDelta:      buffer.readInt8(2)    // Byte 3 as signed int
-  };
-  if (event.leftBtn || event.rightBtn || event.middleBtn) {
-    event.type = 'button';
-  } else {
-    event.type = 'moved';
-  }
-  return event;
-}
-
-function parseKeyboard(dev, buffer) {
-  // /usr/include/linux/input.h:
-  // struct input_event {
-  //     struct timeval time;
-  //     __u16 type;
-  //     __u16 code;
-  //     __s32 value;
-  // };
-  var raw = {
-    type: buffer.readUInt16LE(8),
-    code: buffer.readUInt16LE(10),
-    value: buffer.readInt32LE(12)
-  };
-  var event = dev.pendingEvent || {};
-  switch (raw.type) {
-    case 0x00: // EV_SYN
-      dev.pendingEvent = undefined;
-      return event;
-    case 0x01: // EV_KEY
-      event.type = 'key';
-      event.code = raw.code;
-      event.value = raw.value;
-      break;
-    case 0x04: // EV_MSC
-      if (raw.code == 0x04)
-        event.scancode = raw.value;
-      break;
-    default:
-      console.log(raw);
-  }
-  dev.pendingEvent = event;
-  return undefined;
-}
 
 function InputDevice(dev, bufferSize, parse) {
   this.wrap('onOpen');
@@ -114,16 +57,89 @@ InputDevice.prototype.close = function(callback) {
   this.fd = undefined;
 }
 
+
+// Keyboard
+
+function Keyboard(dev) {
+  function parseKeyboard(dev, buffer) {
+    // /usr/include/linux/input.h:
+    // struct input_event {
+    //     struct timeval time;
+    //     __u16 type;
+    //     __u16 code;
+    //     __s32 value;
+    // };
+    var raw = {
+      type: buffer.readUInt16LE(8),
+      code: buffer.readUInt16LE(10),
+      value: buffer.readInt32LE(12)
+    };
+    var event = dev.pendingEvent || {};
+    switch (raw.type) {
+      case 0x00: // EV_SYN
+        dev.pendingEvent = undefined;
+        return event;
+      case 0x01: // EV_KEY
+        event.type = 'key';
+        event.code = raw.code;
+        event.value = raw.value;
+        break;
+      case 0x04: // EV_MSC
+        if (raw.code == 0x04)
+          event.scancode = raw.value;
+        break;
+      default:
+        console.log(raw);
+    }
+    dev.pendingEvent = event;
+    return undefined;
+  }
+  InputDevice.call(this, dev, 16, parseKeyboard);
+}
+Keyboard.prototype = Object.create(InputDevice.prototype, {
+  constructor: {value: Keyboard}
+});
+
+function Mouse(dev) {
+  // Parse PS/2 mouse protocol
+  // According to http://www.computer-engineering.org/ps2mouse/
+  function parseMouse(dev, buffer) {
+    var event = {
+      leftBtn:    (buffer[0] & 1  ) > 0, // Bit 0
+      rightBtn:   (buffer[0] & 2  ) > 0, // Bit 1
+      middleBtn:  (buffer[0] & 4  ) > 0, // Bit 2
+      xSign:      (buffer[0] & 16 ) > 0, // Bit 4
+      ySign:      (buffer[0] & 32 ) > 0, // Bit 5
+      xOverflow:  (buffer[0] & 64 ) > 0, // Bit 6
+      yOverflow:  (buffer[0] & 128) > 0, // Bit 7
+      xDelta:      buffer.readInt8(1),   // Byte 2 as signed int
+      yDelta:      buffer.readInt8(2)    // Byte 3 as signed int
+    };
+    if (event.leftBtn || event.rightBtn || event.middleBtn) {
+      event.type = 'button';
+    } else {
+      event.type = 'moved';
+    }
+    return event;
+  }
+  InputDevice.call(this, dev, 16, parseMouse);
+}
+Mouse.prototype = Object.create(InputDevice.prototype, {
+  constructor: {value: Mouse}
+});
+
+
+
 /****************
  * Sample Usage *
  ****************/
 
 // read all mouse events from /dev/input/mice
-var mouse = new InputDevice('/dev/input/mice', 3, parseMouse);
+var mouse = new Mouse('/dev/input/mice');
 mouse.on('button', console.log);
 mouse.on('moved', console.log);
 
-var keyboard = new InputDevice('/dev/input/event1', 16, parseKeyboard);
+var keyboard = new Keyboard('/dev/input/event1');
 keyboard.on('key', console.log);
 
 
